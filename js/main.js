e7ad2cd4396a22b03354406def2e73abbb0bbbbc
eslamkhames <eslamkhames7@gmail.com>
2022-08-01 21:18:28 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let up = document.querySelector(".scroll");
+window.onscroll = function () {
+    if(this.scrollY >= 700){
+        up.classList.add("show")
+    }
+    else{
+        up.classList.remove("show")
+    }
+}
+up.onclick = function() {
+    window.scrollTo({
+        top : 0,
+        behavior: "smooth",
+    })
+}
+
+
+
+
+
+
+
+
+
